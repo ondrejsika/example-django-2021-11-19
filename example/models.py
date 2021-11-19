704794b169dbfc4ec3a1f97e2010f9603a5a0439
Ondrej Sika <ondrej@ondrejsika.com>
2021-11-19 09:41:18 +0100

    feat: Create simple model

--- example/models.py
+++ example/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models.base import Model
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return "%s: %s" % (self.author.name, self.title)
