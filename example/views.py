26d2e729fb8abe7ec42a78b4efb7c7d1b09e2b50
Ondrej Sika <ondrej@ondrejsika.com>
2021-11-19 09:55:13 +0100

    feat: Show authors & books on the home page

--- example/views.py
+++ example/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Author, Book
 
 # Create your views here.
 def index_view(request):
-    return render(request, "example/index.html", {})
+    return render(request, "example/index.html", {
+        "authors": Author.objects.all().order_by("name"),
+    })
